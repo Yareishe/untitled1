25232e78194a978e4d802f1fc080bd7e0934c101
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-07-02 16:40:36 +0300

    Create Database.java

---
+++ src/main/java/org/example/Database.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import java.sql.*;
+
+public class Database {
+
+    private static final String DB_URL = "jdbc:h2:tcp://localhost/~/init_db";
+    private static final String DB_USERNAME = "sa";
+    private static final String DB_PASS = "";
+
+    private static Database instance;
+    private Connection connection;
+
+    public static synchronized Database getInstance() {
+        if (instance == null) {
+            instance = new Database();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASS);
+        }
+        return connection;
+    }
+
+    public void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Database database = Database.getInstance();
+            Connection con = database.getConnection();
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM worker");
+            if (rs.next()) {
+                String name = rs.getString("name");
+                System.out.println(name);
+            }
+            database.closeConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
