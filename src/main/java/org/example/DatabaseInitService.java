31a971ca2bc7d57debe4a2284fd02a4d44a63335
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-07-05 03:08:16 +0300

    Update DatabaseInitService.java

--- src/main/java/org/example/DatabaseInitService.java
+++ src/main/java/org/example/DatabaseInitService.java
@@ -1,42 +1,38 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseInitService {
-    private static final String DB_URL = "jdbc:h2:file:C:/data/INIT_DB";
-    private static final String DB_USERNAME = "";
-    private static final String DB_PASSWORD = "";
-    private static final String SQL_FILE_PATH = "D:\\untitled1\\all.sql\\init_db.sql";
 
     public static void main(String[] args) {
         try {
-            Connection connection = getConnection();
-            executeSqlFile(connection, SQL_FILE_PATH);
-            connection.close();
+            Database database = Database.getInstance();
+            Connection connection = database.getConnection();
+            executeSqlFile(connection, "sql/init_db.sql");
+            database.closeConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    private static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-    }
-
     private static void executeSqlFile(Connection connection, String filePath) {
-        try {
-            Path path = Paths.get(filePath);
-            String sqlScript = new String(Files.readAllBytes(path));
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath));
+             Statement statement = connection.createStatement()) {
+            StringBuilder sql = new StringBuilder();
+            String line;
 
-            Statement statement = connection.createStatement();
-            statement.execute(sqlScript);
-            statement.close();
+            while ((line = reader.readLine()) != null) {
+                sql.append(line);
+                if (line.endsWith(";")) {
+                    statement.execute(sql.toString());
+                    sql.setLength(0);
+                }
+            }
         } catch (IOException | SQLException e) {
             e.printStackTrace();
         }
