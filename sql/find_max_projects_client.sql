85ea7dd2a4109fd6ab91fd823ae97e40a93a1b99
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-07-06 03:55:48 +0300

    Create find_max_projects_client.sql

---
+++ sql/find_max_projects_client.sql
@@ -0,0 +1,13 @@
+SELECT c.NAME, COUNT(c.ID) AS PROJECT_COUNT
+FROM client c
+         JOIN project p ON c.ID = p.CLIENT_ID
+GROUP BY c.NAME
+HAVING COUNT(p.ID) = (
+    SELECT MAX(project_count)
+    FROM (
+             SELECT COUNT(p.ID) AS project_count
+             FROM client c
+                      JOIN project p ON c.ID = p.CLIENT_ID
+             GROUP BY c.NAME
+         ) AS counts
+);
