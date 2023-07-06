ba72f7123e6027e29b2b57d64abcf7adb4c4bd86
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-07-06 03:54:43 +0300

    Delete PROJECT_WORKER.sql

--- db/PUBLIC/PROJECT_WORKER.sql
+++
@@ -1,9 +0,0 @@
-create table PROJECT_WORKER
-(
-    PROJECT_ID INTEGER not null
-        references PROJECT,
-    WORKER_ID  INTEGER not null
-        references WORKER,
-    primary key (PROJECT_ID, WORKER_ID)
-);
-
